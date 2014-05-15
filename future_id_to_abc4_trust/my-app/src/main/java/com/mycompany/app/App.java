76daaf493760f317211de1afeb1d537f3da22511
adelapie <A.delaPiedra@cs.ru.nl>
2014-05-15 15:21:14 +0200

    prototype for the presentation policy writer

--- future_id_to_abc4_trust/my-app/src/main/java/com/mycompany/app/App.java
+++ future_id_to_abc4_trust/my-app/src/main/java/com/mycompany/app/App.java
@@ -2,6 +2,9 @@ package com.mycompany.app;
 
 import java.io.File;
 import java.util.List;
+import java.io.FileWriter;
+import java.io.StringWriter;
+
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
@@ -10,19 +13,33 @@ import org.jaxen.JaxenException;
 import org.jaxen.XPath;
 import org.jaxen.dom4j.Dom4jXPath;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
 
-    // XXXX: It must be an string.
+  public static final String requestedAttributes = "/home/vmr/second_future_id_abc_4_trust/p2abcengine-master/Code/core-abce/abce-services/tutorial-resources/future_id_to_abc4_trust/my-app/src/main/java/com/mycompany/app/att.xml";
+  public static final String presentationPolicyDest = "/home/vmr/second_future_id_abc_4_trust/p2abcengine-master/Code/core-abce/abce-services/tutorial-resources/future_id_to_abc4_trust/my-app/src/main/java/com/mycompany/app/presentationPolicyAlternatives.xml";
 
-  File xmlFile = new File("/home/vmr/second_future_id_abc_4_trust/p2abcengine-master/Code/core-abce/abce-services/tutorial-resources/future_id_to_abc4_trust/my-app/src/main/java/com/mycompany/app/att.xml");
+  public static final String policyFirstPart = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" +
+                                               "<abc:PresentationPolicyAlternatives xmlns:abc=\"http://abc4trust.eu/wp2/abcschemav1.0\"" +
+                                               " Version=\"1.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"" +
+                                               " xsi:schemaLocation=\"http://abc4trust.eu/wp2/abcschemav1.0 ../../../../../../../../abc4trust-xml/src/main/resources/xsd/schema.xsd\">" +
+                                               "<abc:PresentationPolicy PolicyUID=\"http://MyFavoriteSoccerTeam/policies/match/842/vip\">" +
+                                               "<abc:Message><abc:ApplicationData>MyFavoriteSoccerTeam vs. OtherTeam</abc:ApplicationData></abc:Message>" +
+                                               "<abc:Credential Alias=\"#ticket\"><abc:CredentialSpecAlternatives><abc:CredentialSpecUID>http://MyFavoriteSoccerTeam/tickets/vip</abc:CredentialSpecUID>" +
+                                               "</abc:CredentialSpecAlternatives><abc:IssuerAlternatives><abc:IssuerParametersUID>http://ticketcompany/MyFavoriteSoccerTeam/issuance:idemix</abc:IssuerParametersUID></abc:IssuerAlternatives>";
+                                              
+  public static final String policySecondPart = "</abc:Credential></abc:PresentationPolicy></abc:PresentationPolicyAlternatives>";
+  
+  public static void main( String[] args ) {
+
+  File xmlFile = new File(requestedAttributes);
   SAXReader reader = new SAXReader();
  
   Document dom4jDocument = null;
   XPath path = null;
   List<Element> results = null;
+
+  StringWriter sw = new StringWriter();
+  sw.write(policyFirstPart);
  
   try {
  
@@ -30,8 +47,11 @@ public class App
    path = new Dom4jXPath("Attributes/Attribute/ID");
    results = path.selectNodes(dom4jDocument);
  
+   
    for (Element element : results) {
-    System.out.println("<abc:DisclosedAttribute AttributeType=\"" + element.getData() + "\" " + "DataHandlingPolicy=\"http://www.sweetdreamsuites.com/policies/creditcards\"/>");
+     String disclosedAttribute = new String("<abc:DisclosedAttribute AttributeType=\"" + element.getData() + "\" " + "DataHandlingPolicy=\"http://www.sweetdreamsuites.com/policies/creditcards\"/>");
+     sw.write(disclosedAttribute);
+     sw.write("\n");
    }
  
   } catch (JaxenException e) {
@@ -40,48 +60,21 @@ public class App
    e.printStackTrace();
   }
 
-// 1st part
+  
+  sw.write(policySecondPart);
+  System.out.println(sw.toString());
 
-/*
+  try {
 
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+  FileWriter fw = new FileWriter(presentationPolicyDest);
+  fw.write(sw.toString());
+  fw.close();
 
-<!-- This is a sample ABC4Trust presentation policy for... -->
+  } catch (Exception e) {
+   e.printStackTrace();
+  }
 
-<abc:PresentationPolicyAlternatives
-  xmlns:abc="http://abc4trust.eu/wp2/abcschemav1.0"
-  Version="1.0"
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xsi:schemaLocation="http://abc4trust.eu/wp2/abcschemav1.0 ../../../../../../../../abc4trust-xml/src/main/resources/xsd/schema.xsd">
-  <abc:PresentationPolicy
-    PolicyUID="http://MyFavoriteSoccerTeam/policies/match/842/vip">
-    <abc:Message>
-      <abc:ApplicationData>
-        MyFavoriteSoccerTeam vs. OtherTeam
-      </abc:ApplicationData>
-    </abc:Message>
-    <abc:Credential Alias="#ticket">
-      <abc:CredentialSpecAlternatives>
-        <abc:CredentialSpecUID>http://MyFavoriteSoccerTeam/tickets/vip</abc:CredentialSpecUID>
-      </abc:CredentialSpecAlternatives>
-      <abc:IssuerAlternatives>
-        <abc:IssuerParametersUID>http://ticketcompany/MyFavoriteSoccerTeam/issuance:idemix</abc:IssuerParametersUID>
-      </abc:IssuerAlternatives>
 
-DISCLOSURES---
-*/
-
-/*
-
- 2nd part
-
-    </abc:Credential>
-  </abc:PresentationPolicy>
-</abc:PresentationPolicyAlternatives>
-
-*/
-
-   
 
     }
 }

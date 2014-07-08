0df2227ca3113ad0fceba7dcf002fa4c5db1995d
a.delapiedra@cs.ru.nl <a.delapiedra@cs.ru.nl>
2014-07-08 14:39:11 +0200

    Uploaded abc4trust trigger

---
+++ abc_trigger/README.md
@@ -0,0 +1,5 @@
+
+Simple trigger for ABC4Trust in the Open ecard App.
+
+Instructions: make && load via MUtil.
+
---
+++ abc_trigger/src/ABC4TrustTrigger.c
@@ -0,0 +1,20 @@
+/**
+ * ABC4TrustTrigger.c
+ *
+ * This configuration is used for triggering the
+ * ABC4Trust protocol in the Open eCard App. It does
+ * nothing more.
+ */
+
+#pragma attribute("aid", "F8 41 42 43 34 74 72 75 73")
+#pragma attribute("dir", "61 15 4F 09 F8 41 42 43 34 74 72 75 73 50 08 49 52 4D 41 63 61 72 64")
+#pragma attribute("fci", "6F 16 A5 14 10 12 02 01 00 02 01 08 10 0A 0C 05 61 6C 70 68 61 02 01 00")
+
+#pragma melpublic
+#pragma melsession
+#pragma melstatic
+
+
+void main(void) {
+  /* do nothing */
+}
